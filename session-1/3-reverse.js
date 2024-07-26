c8b6c652a1758859ae7d5320d383831e9e110c1b
Carl Angelo Cabahug <ca.cabahug@gmail.com>
2024-07-26 15:23:02 +0800

    Updated reverse function to use for loop

--- session-1/3-reverse.js
+++ session-1/3-reverse.js
@@ -1,7 +1,15 @@
 function reverse(word){
-    return word.split('')
-    .reverse()
-    .join('')
+    // return word.split('')
+    // .reverse()
+    // .join('')
+    
+    let invertedString = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+    invertedString += word[i];
+    }
+    return invertedString;
+      
+
 }
 
 console.log(reverse('hello'))
